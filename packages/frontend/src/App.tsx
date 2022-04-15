dfe671fb1e8051df7a97df01c804cce5eed91dae
j1998719 <j1998719@gmail.com>
2022-04-15 23:24:06 +0800

    tired

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -15,6 +15,7 @@ import logo from './images/logo.png'
 import bombClubBlack from './images/bombClubBlack.jpg'
 import GSMN2 from "./images/GSMN2.png"
 import GSMN3 from "./images/GSMN3.png"
+import S__126140929 from "./images/S__126140929.jpg"
 
 
 import { TwitterSquareFilled, InstagramFilled } from '@ant-design/icons';
@@ -274,20 +275,226 @@ function App() {
           </Col>
         </section>
       </div>
-      <div>
-        <div>
+      <section style={{ display: 'flex', flexDirection: 'column', background: '#000000', color: '#ffffff', padding: '120px 200px' }}>
 
-        </div>
         <div>
-          <div>
-            <div>
-              <div></div>
-            </div>
-            <div></div>
-            <div></div>
+          <h1 style={{ color: '#12c2e9', letterSpacing: '10px', marginBottom: '20px', marginLeft: '16px', fontSize: '30px', textTransform: 'uppercase' }}> Roadmap</h1>
+        </div>
+        <div style={{ backgroundColor: '#232323', display: 'flex', flexDirection: 'column', justifyContent: "center", alignItems: 'center' }}>
+          <div style={{ margin: '16px' }}>
+            <div style={{ fontSize: "60px", fontWeight: 700, color: "transparent", WebkitTextStroke: "1px #b19777", marginLeft: '16px' }}>01</div>
+            <div style={{ color: "#ffffff", letterSpacing: "1px", lineHeight: 1.4, fontWeight: 500, fontSize: '25px', marginLeft: '16px', margin: '12px' }}>GALOSSEUM GENESIS COLLECTION</div>
+            <p style={{ color: "#ffffff", marginLeft: '24px' }}>We are planting our roots at this stage while we work on perfecting our galosseum. As our first goal involves a successful drop in order to let our early supporters to own a fighter soul and provide other investors opportunities for future investments.
+            </p>
+            <p style={{ color: "#ffffff", marginLeft: '24px' }}>The GSMN project is a long term project built with many individual aspects included such as art, design, game, technology and quality control elements. We strive to guarantee our communities the finest results possible.
+            </p>
+            <p style={{ color: "#ffffff", marginLeft: '24px' }}>When we’ll be able to proudly stand behind our promise for the GSMN community we will announce the utilities for the first drop of our project, so they can enjoy all the benefits and effort that we have put in each and every GSMN fighter.
+            </p>
+          </div>
+          <div style={{ margin: '16px' }}>
+            <div style={{ fontSize: "60px", fontWeight: 700, color: "transparent", WebkitTextStroke: "1px #b19777", marginLeft: '16px' }}>02</div>
+            <div style={{ color: "#ffffff", letterSpacing: "1px", lineHeight: 1.4, fontWeight: 500, fontSize: '25px', marginLeft: '16px', margin: '12px' }}>GALOSSEUM GENESIS COLLECTION</div>
+            <p style={{ color: "#ffffff", marginLeft: '24px' }}>We are planting our roots at this stage while we work on perfecting our galosseum. As our first goal involves a successful drop in order to let our early supporters to own a fighter soul and provide other investors opportunities for future investments.
+            </p>
+            <p style={{ color: "#ffffff", marginLeft: '24px' }}>The GSMN project is a long term project built with many individual aspects included such as art, design, game, technology and quality control elements. We strive to guarantee our communities the finest results possible.
+            </p>
+            <p style={{ color: "#ffffff", marginLeft: '24px' }}>When we’ll be able to proudly stand behind our promise for the GSMN community we will announce the utilities for the first drop of our project, so they can enjoy all the benefits and effort that we have put in each and every GSMN fighter.
+            </p>
+          </div>
+          <div style={{ margin: '16px' }}>
+            <div style={{ fontSize: "60px", fontWeight: 700, color: "transparent", WebkitTextStroke: "1px #b19777", marginLeft: '16px' }}>03</div>
+            <div style={{ color: "#ffffff", letterSpacing: "1px", lineHeight: 1.4, fontWeight: 500, fontSize: '25px', marginLeft: '16px', margin: '12px' }}>GALOSSEUM GENESIS COLLECTION</div>
+            <p style={{ color: "#ffffff", marginLeft: '24px' }}>We are planting our roots at this stage while we work on perfecting our galosseum. As our first goal involves a successful drop in order to let our early supporters to own a fighter soul and provide other investors opportunities for future investments.
+            </p>
+            <p style={{ color: "#ffffff", marginLeft: '24px' }}>The GSMN project is a long term project built with many individual aspects included such as art, design, game, technology and quality control elements. We strive to guarantee our communities the finest results possible.
+            </p>
+            <p style={{ color: "#ffffff", marginLeft: '24px' }}>When we’ll be able to proudly stand behind our promise for the GSMN community we will announce the utilities for the first drop of our project, so they can enjoy all the benefits and effort that we have put in each and every GSMN fighter.
+            </p>
+          </div>
+          <div style={{ margin: '16px' }}>
+            <div style={{ fontSize: "60px", fontWeight: 700, color: "transparent", WebkitTextStroke: "1px #b19777", marginLeft: '16px' }}>04</div>
+            <div style={{ color: "#ffffff", letterSpacing: "1px", lineHeight: 1.4, fontWeight: 500, fontSize: '25px', marginLeft: '16px', margin: '12px' }}>GALOSSEUM GENESIS COLLECTION</div>
+            <p style={{ color: "#ffffff", marginLeft: '24px' }}>We are planting our roots at this stage while we work on perfecting our galosseum. As our first goal involves a successful drop in order to let our early supporters to own a fighter soul and provide other investors opportunities for future investments.
+            </p>
+            <p style={{ color: "#ffffff", marginLeft: '24px' }}>The GSMN project is a long term project built with many individual aspects included such as art, design, game, technology and quality control elements. We strive to guarantee our communities the finest results possible.
+            </p>
+            <p style={{ color: "#ffffff", marginLeft: '24px' }}>When we’ll be able to proudly stand behind our promise for the GSMN community we will announce the utilities for the first drop of our project, so they can enjoy all the benefits and effort that we have put in each and every GSMN fighter.
+            </p>
+          </div>
+          <div style={{ margin: '16px' }}>
+            <div style={{ fontSize: "60px", fontWeight: 700, color: "transparent", WebkitTextStroke: "1px #b19777", marginLeft: '16px' }}>05</div>
+            <div style={{ color: "#ffffff", letterSpacing: "1px", lineHeight: 1.4, fontWeight: 500, fontSize: '25px', marginLeft: '16px', margin: '12px' }}>GALOSSEUM GENESIS COLLECTION</div>
+            <p style={{ color: "#ffffff", marginLeft: '24px' }}>We are planting our roots at this stage while we work on perfecting our galosseum. As our first goal involves a successful drop in order to let our early supporters to own a fighter soul and provide other investors opportunities for future investments.
+            </p>
+            <p style={{ color: "#ffffff", marginLeft: '24px' }}>The GSMN project is a long term project built with many individual aspects included such as art, design, game, technology and quality control elements. We strive to guarantee our communities the finest results possible.
+            </p>
+            <p style={{ color: "#ffffff", marginLeft: '24px' }}>When we’ll be able to proudly stand behind our promise for the GSMN community we will announce the utilities for the first drop of our project, so they can enjoy all the benefits and effort that we have put in each and every GSMN fighter.
+            </p>
+          </div>
+          <div style={{ margin: '16px' }}>
+            <div style={{ fontSize: "60px", fontWeight: 700, color: "transparent", WebkitTextStroke: "1px #b19777", marginLeft: '16px' }}>06</div>
+            <div style={{ color: "#ffffff", letterSpacing: "1px", lineHeight: 1.4, fontWeight: 500, fontSize: '25px', marginLeft: '16px', margin: '12px' }}>GALOSSEUM GENESIS COLLECTION</div>
+            <p style={{ color: "#ffffff", marginLeft: '24px' }}>We are planting our roots at this stage while we work on perfecting our galosseum. As our first goal involves a successful drop in order to let our early supporters to own a fighter soul and provide other investors opportunities for future investments.
+            </p>
+            <p style={{ color: "#ffffff", marginLeft: '24px' }}>The GSMN project is a long term project built with many individual aspects included such as art, design, game, technology and quality control elements. We strive to guarantee our communities the finest results possible.
+            </p>
+            <p style={{ color: "#ffffff", marginLeft: '24px' }}>When we’ll be able to proudly stand behind our promise for the GSMN community we will announce the utilities for the first drop of our project, so they can enjoy all the benefits and effort that we have put in each and every GSMN fighter.
+            </p>
           </div>
         </div>
+      </section>
+      <div style={{ backgroundColor: '#000000', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignContent: 'center' }}>
+        <Row style={{ justifyContent: 'center', alignContent: 'center' }}>
+          <p style={{ color: "#ffffff", textTransform: "uppercase", letterSpacing: "7px" }}>OUR TEAM</p>
+        </Row>
+        <Row style={{ display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
+          <p style={{ color: "#ffffff", width: '800px', fontWeight: 300, fontSize: "14px", textTransform: "uppercase", letterSpacing: "7px", marginBottom: "10px", textAlign: 'center' }}>OUR TEAM IS COMPOSED WITH ARTISTS, INVESTORS, ENTREPRENEURS, NFT SPECIALISTS AND BLOCKCHAIN EXPERTS. A DIVERSIFIED TEAM OF PROFESSIONALS SPECIALISED IN THE WEB 3.0.</p>
+        </Row>
       </div>
+      <div style={{ backgroundColor: '#000000', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignContent: 'center', paddingTop: '64px' }}>
+        <Row style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly' }}>
+          <img src={S__126140929} alt='PFP' height='200px' />
+          <img src={S__126140929} alt='PFP' height='200px' />
+          <img src={S__126140929} alt='PFP' height='200px' />
+          <img src={S__126140929} alt='PFP' height='200px' />
+        </Row>
+
+      </div>
+      <section style={{ backgroundColor: '#000000', display: 'flex', flexDirection: 'row', paddingTop: '100px', paddingLeft: '120px', paddingRight: '120px' }}>
+        <Col span={6} offset={2} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'start', alignContent: 'center', marginRight: '40px' }}>
+          <div style={{ position: "relative" }}>
+            <h6 style={{ color: '#ffffff', background: "linear-gradient(to right, #12c2e9, #c471ed, #f64f59)", padding: "7px 12px", borderRadius: "10px", display: 'inline-block', marginBottom: '15px' }}>Question and Answers</h6>
+          </div>
+          <div style={{ textAlign: 'left' }}>
+            <h3 style={{ display: 'block', color: "#ffffff", fontWeight: 600, marginBottom: '5px', lineHeight: 1.4, fontSize: '35px' }}>Have</h3>
+            <h3 style={{ display: 'block', color: "#ffffff", fontWeight: 600, marginBottom: '5px', lineHeight: 1.4, fontSize: '35px' }}> Question?</h3>
+          </div>
+          <div style={{ marginTop: '40px', background: '#000000', marginBottom: '50px' }}>
+            <span style={{ display: 'inline-block', letterSpacing: '10px', fontSize: '12px', color: '#ffffff', fontWeight: 400, paddingBottom: '10px' }}>JOIN US NOW</span>
+            <div style={{}}>
+              <div style={{ background: 'linear-gradient(to right, #12c2e9, #c471ed, #f64f59)', height: '1px', width: '40%' }}>
+              </div>
+            </div>
+          </div>
+
+        </Col>
+        <Col span={16} offset={2} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'start', alignContent: 'start', paddingRight: '120px' }}>
+          <div style={{ display: 'flex' }}>
+            <div style={{ display: 'flex' }}>
+              <div style={{ display: 'block' }}>
+                <h5 style={{ color: '#ffffff', fontSize: 22, fontWeight: 500 }}>
+                  1. When can I mint a GSMN?
+                </h5>
+                <h6 style={{ color: '#ffffff', boxSizing: 'border-box', marginBottom: '20px', lineHeight: 1.5, fontWeight: 400, fontSize: 14 }}>
+                  A pre-sale will be available for our early supporters on Twitter, Discord and Instagram. Details to be confirmed.
+                </h6>
+                <div style={{ borderBottom: '1px solid rgba(255, 255, 255, 0.2)' }}></div>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', marginTop: 30 }}>
+            <div style={{ display: 'flex' }}>
+              <div style={{ display: 'block' }}>
+                <h5 style={{ color: '#ffffff', fontSize: 22, fontWeight: 500 }}>
+                  2. When can I mint a GSMN?
+                </h5>
+                <h6 style={{ color: '#ffffff', boxSizing: 'border-box', marginBottom: '20px', lineHeight: 1.5, fontWeight: 400, fontSize: 14 }}>
+                  A pre-sale will be available for our early supporters on Twitter, Discord and Instagram. Details to be confirmed.
+                </h6>
+                <div style={{ borderBottom: '1px solid rgba(255, 255, 255, 0.2)' }}></div>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', marginTop: 30 }}>
+            <div style={{ display: 'flex' }}>
+              <div style={{ display: 'block' }}>
+                <h5 style={{ color: '#ffffff', fontSize: 22, fontWeight: 500 }}>
+                  3. When can I mint a GSMN?
+                </h5>
+                <h6 style={{ color: '#ffffff', boxSizing: 'border-box', marginBottom: '20px', lineHeight: 1.5, fontWeight: 400, fontSize: 14 }}>
+                  A pre-sale will be available for our early supporters on Twitter, Discord and Instagram. Details to be confirmed.
+                </h6>
+                <div style={{ borderBottom: '1px solid rgba(255, 255, 255, 0.2)' }}></div>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', marginTop: 30 }}>
+            <div style={{ display: 'flex' }}>
+              <div style={{ display: 'block' }}>
+                <h5 style={{ color: '#ffffff', fontSize: 22, fontWeight: 500 }}>
+                  4. When can I mint a GSMN?
+                </h5>
+                <h6 style={{ color: '#ffffff', boxSizing: 'border-box', marginBottom: '20px', lineHeight: 1.5, fontWeight: 400, fontSize: 14 }}>
+                  A pre-sale will be available for our early supporters on Twitter, Discord and Instagram. Details to be confirmed.
+                </h6>
+                <div style={{ borderBottom: '1px solid rgba(255, 255, 255, 0.2)' }}></div>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', marginTop: 30 }}>
+            <div style={{ display: 'flex' }}>
+              <div style={{ display: 'block' }}>
+                <h5 style={{ color: '#ffffff', fontSize: 22, fontWeight: 500 }}>
+                  5. When can I mint a GSMN?
+                </h5>
+                <h6 style={{ color: '#ffffff', boxSizing: 'border-box', marginBottom: '20px', lineHeight: 1.5, fontWeight: 400, fontSize: 14 }}>
+                  A pre-sale will be available for our early supporters on Twitter, Discord and Instagram. Details to be confirmed.
+                </h6>
+                <div style={{ borderBottom: '1px solid rgba(255, 255, 255, 0.2)' }}></div>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', marginTop: 30 }}>
+            <div style={{ display: 'flex' }}>
+              <div style={{ display: 'block' }}>
+                <h5 style={{ color: '#ffffff', fontSize: 22, fontWeight: 500 }}>
+                  6. When can I mint a GSMN?
+                </h5>
+                <h6 style={{ color: '#ffffff', boxSizing: 'border-box', marginBottom: '12px', lineHeight: 1.5, fontWeight: 400, fontSize: 14 }}>
+                  A pre-sale will be available for our early supporters on Twitter, Discord and Instagram. Details to be confirmed.
+                </h6>
+                <div style={{ borderBottom: '1px solid rgba(255, 255, 255, 0.2)' }}></div>
+              </div>
+            </div>
+          </div>
+        </Col>
+
+      </section>
+      <section style={{ backgroundColor: '#000000' }}>
+        <div style={{ margin: 200, display: "flex", justifyContent: "center", alignContent: 'center', paddingLeft: 15, paddingRight: 15 }}>
+          <Col span={8}>
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <p style={{ color: "#ffffff", marginBottom: 20, fontSize: 30, fontWeight: 20 }}>Social</p>
+              <p style={{ display: 'flex', justifyContent: 'start', lineHeight: 1.4, gap: '10px' }}>
+                <a href={INSTAGRAM_LINK} target="_blank" style={{ fontSize: '32px', color: "#ffffff" }}>
+                  <InstagramFilled />
+                </a>
+                <a href={TWITTER_LINK} target="_blank" style={{ fontSize: '32px', color: "#ffffff" }}>
+                  <TwitterSquareFilled />
+                </a>
+              </p>
+            </div>
+          </Col>
+
+          <Col span={8} offset={8}>
+            <div style={{ display: "flex", flexDirection: 'column' }}>
+              <div style={{ display: 'flex', flexDirection: 'row', gap: '10px', alignItems: 'center' }}>
+                <img src={bombClubBlack} alt='bombClubBlack' height='30px' />
+                <p style={{ color: "#ffffff", fontSize: 30, fontWeight: 20 }} >BombClub</p>
+              </div>
+              <div style={{ display: "flex", flexDirection: 'row', justifyContent: 'flex-end' }}>
+                <p style={{ color: "#ffffff", fontSize: 30, fontWeight: 20 }} >BombClub</p>
+
+              </div>
+
+
+
+            </div>
+          </Col>
+
+        </div>
+      </section>
+
 
     </Layout >
   );
